83b7a21bfb50384526f75416266fbbd9cd5713e7
Jaspreet Singh <jaspreetsingh6090@gmail.com>
2024-07-05 00:51:05 +0530

    changes

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,25 +1,24 @@
 import 'dotenv/config';
-
-import{connectDB} from "./db/index.js"
+import { connectDB } from "./db/index.js";
 import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
 
-connectDB().then(() => {
-  app.get("/",(req,res)=>{
-    res.send("Hello World");
-  })
-  
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
 
-  app.on("error",()=>{
+    app.on("error", (error) => {
+      console.log("Error connecting to MongoDB:", error);
+    });
+    
+    app.get("/", (req, res) => {
+      res.send("Hello World");
+    });
+
+  })
+  .catch((error) => {
     console.log("Error connecting to MongoDB:", error);
-  })
-}).catch((error)=>{
-  console.log("Error connecting to MongoDB:", error);
-})
-
-
-
+  });
